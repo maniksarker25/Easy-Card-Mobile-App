0a50fe091b017b98b27a1298237a40728c73ed89
Manik <tonysins43@gmail.com>
2023-11-21 01:49:53 +0600

    make multi card system

--- lib/root.dart
+++ lib/root.dart
@@ -1,4 +1,61 @@
-// MyComponent.dart
+// import 'dart:convert';
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+// import 'package:my_app/feildInfo.dart';
+// import 'profile.dart';
+// import 'profileInfo.dart';
+
+// class MyComponent extends StatefulWidget {
+//   @override
+//   _MyComponentState createState() => _MyComponentState();
+// }
+
+// class _MyComponentState extends State<MyComponent> {
+//   List<Map<String, dynamic>> profiles = [];
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     fetchData();
+//   }
+
+//   Future<void> fetchData() async {
+//     final response = await http.get(Uri.parse(
+//         'https://business-card-backend-2.vercel.app/cards/cards/jack@gmail.com'));
+
+//     if (response.statusCode == 200) {
+//       setState(() {
+//         profiles = List<Map<String, dynamic>>.from(json.decode(response.body));
+//       });
+//     } else {
+//       throw Exception('Failed to load data');
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SingleChildScrollView(
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: profiles.map((profile) {
+//           return Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               ProfileComponent(profile: profile),
+//               ProfileInfo(profile: profile),
+//               FieldInfo(profile: profile),
+//               // You can add other components/widgets that require profile data
+//               // Text(profile['email'] ?? 'No email available'),
+//               SizedBox(height: 20), // Spacer between profiles
+//             ],
+//           );
+//         }).toList(),
+//       ),
+//     );
+//   }
+// }
+
+// 00000000000000000000000000000000000000000
 
 import 'dart:convert';
 import 'package:flutter/material.dart';
@@ -13,7 +70,9 @@ class MyComponent extends StatefulWidget {
 }
 
 class _MyComponentState extends State<MyComponent> {
-  Map<String, dynamic> profile = {};
+  List<Map<String, dynamic>> profiles = [];
+  PageController _pageController = PageController(initialPage: 0);
+  int _currentPage = 0;
 
   @override
   void initState() {
@@ -23,16 +82,70 @@ class _MyComponentState extends State<MyComponent> {
 
   Future<void> fetchData() async {
     final response = await http.get(Uri.parse(
-        'https://business-card-backend-2.vercel.app/cards/visit/c78cdk0'));
+        'https://business-card-backend-2.vercel.app/cards/cards/maniksarker265@gmail.com'));
 
     if (response.statusCode == 200) {
       setState(() {
-        profile = json.decode(response.body);
+        profiles = List<Map<String, dynamic>>.from(json.decode(response.body));
       });
     } else {
       throw Exception('Failed to load data');
     }
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: profiles.isEmpty
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : PageView.builder(
+              itemCount: profiles.length,
+              controller: _pageController,
+              onPageChanged: (int index) {
+                setState(() {
+                  _currentPage = index;
+                });
+              },
+              itemBuilder: (BuildContext context, int index) {
+                return ProfilePage(profile: profiles[index]);
+              },
+            ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          if (_currentPage > 0)
+            FloatingActionButton(
+              onPressed: () {
+                _pageController.previousPage(
+                  duration: Duration(milliseconds: 300),
+                  curve: Curves.easeInOut,
+                );
+              },
+              child: Icon(Icons.arrow_back),
+            ),
+          SizedBox(width: 10),
+          if (_currentPage < profiles.length - 1)
+            FloatingActionButton(
+              onPressed: () {
+                _pageController.nextPage(
+                  duration: Duration(milliseconds: 300),
+                  curve: Curves.easeInOut,
+                );
+              },
+              child: Icon(Icons.arrow_forward),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  final Map<String, dynamic> profile;
+
+  ProfilePage({required this.profile});
 
   @override
   Widget build(BuildContext context) {
@@ -43,8 +156,7 @@ class _MyComponentState extends State<MyComponent> {
           ProfileComponent(profile: profile),
           ProfileInfo(profile: profile),
           FieldInfo(profile: profile),
-          // Display the fetched profiles
-          // Text(profile['email'] ?? 'No email available'),
+          // Other components/widgets using profile data
         ],
       ),
     );
