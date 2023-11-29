d976f78af348f404f00676c1153cf74ddb22ead8
shafi0072 <safi29317@gmail.com>
2023-11-30 01:19:19 +0600

    shafi done

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'root.dart';
 import 'anotherPage.dart';
 import 'login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
